827639901c472da0e5689c0acfbe1c2c2623d1e0
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2022-04-17 11:21:44 +1000

    feat: add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { RegisterRoutes } from "./generated/routes";
 
 export const app = express();
+
+app.use(cors({ origin: "*" }));
 
 app.use(
   bodyParser.urlencoded({
